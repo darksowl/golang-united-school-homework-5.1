9bfe638883f1a92c5efb545b8f621f5f148e371d
Natalya <trotsyuknata@yandex.ru>
2022-05-30 21:19:21 +0300

    homework

--- square.go
+++ square.go
@@ -1,4 +1,9 @@
 package square
+//package main
+
+//import (
+//	"fmt"
+//)
 
 type Point struct {
 	x, y int
@@ -9,14 +14,30 @@ type Square struct {
 	a     uint
 }
 
-func (receiver) End() Point {
+func (s Square) End() Point {
 	// implement me
+	var e Point
+	e.x = s.start.x + int(s.a)
+	e.y = s.start.y + int(s.a)
+	return e
 }
 
-func (receiver) Area() uint {
+func (s Square) Area() uint {
 	// implement me
+	return s.a * s.a
 }
 
-func (receiver) Perimeter() uint {
+func (s Square) Perimeter() uint {
 	// implement me
+	return s.a * 4
 }
+
+//func main(){
+//	var s Square = Square{start: Point{x: -2, y: 4}, a: 0}
+//	e := s.End()
+//	fmt.Println(e)
+//	a := s.Area()
+//	fmt.Println(a)
+//	p := s.Perimeter()
+//	fmt.Println(p)
+//}
